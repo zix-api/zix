14ebf859a48c515004daf62f36f6a5d2ba1c06b3
VoltagedDebunked <rusindanilo@gmail.com>
2024-10-11 17:24:44 +0100

    Init

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let header_path = "raylib.h"; // Path to your C header file
+
+    let bindings = bindgen::Builder::default()
+        .header(header_path)
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+}
